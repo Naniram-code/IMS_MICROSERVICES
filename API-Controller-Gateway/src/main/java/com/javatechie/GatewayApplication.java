089d5a0824c6babbdff3e6430c0ae81bc65f5aa7
Naniram-code <NaniramBhujel@gmail.com>
2023-07-30 21:54:02 -0400

    GateWay Security finall  edited  code ADDED

--- API-Controller-Gateway/src/main/java/com/javatechie/GatewayApplication.java
+++ API-Controller-Gateway/src/main/java/com/javatechie/GatewayApplication.java
@@ -7,11 +7,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 
-	public class SwiggyGatewayApplication {
+	public class GatewayApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SwiggyGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
